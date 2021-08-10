47d71e7970ad64d25615155c054bddad51604272
bipan82gill <56560207+bipan82gill@users.noreply.github.com>
2021-08-09 21:38:49 -0400

    setup basic structure for app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import './App.css';
+import Header from './components/Header/Header';
+import List from './components/List/List';
+import Map from './components/Map/Map';
 
 function App() {
   return (
